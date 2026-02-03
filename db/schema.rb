48a7ac46c4904c40107585e53b145a88c3d7425a
Tristan Dunn <hello@tristandunn.com>
2026-02-02 21:05:20 -0500

    Update dependencies.
    * Updates Ruby to 4.0.1.
    * Updates Node to 24.13.0.
    * Updates Yarn to 4.12.0.
    * Updates bootsnap, brakeman, bundler, faker, listen, puma, rubocop, ruby_llm,
      selenium-webdriver, simplecov-console, solid_queue, thruster, and turbo-rails
      in Ruby.
    * Updates @stylistic/eslint-plugin, jsdom, stylelint, and
      stylelint-config-standard in JavaScript.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_01_051312) do
+ActiveRecord::Schema[8.1].define(version: 2025_06_01_051312) do
   create_table "bookmarks", force: :cascade do |t|
-    t.string "title", limit: 255, null: false
-    t.string "url", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "summary"
+    t.string "title", limit: 255, null: false
+    t.datetime "updated_at", null: false
+    t.string "url", null: false
     t.index ["url"], name: "index_bookmarks_on_url", unique: true
   end
 end
